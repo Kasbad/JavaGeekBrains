1b61ebaf28a4a70f40789e0149c5fa15f783be60
Kasbad <60070800+Kasbad@users.noreply.github.com>
2020-01-31 00:57:58 +0600

    Д/З_5

---
+++ Bird.java
@@ -0,0 +1,7 @@
+package ru.geekbreans.lesson5;
+
+public class Bird extends Animal {
+    public Bird(int flee,int sail,float jump, int age, int ageLife) {
+        super(flee, sail, jump, age, ageLife);
+    }
+}
